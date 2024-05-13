812e81e384bbe42e41c3aff5a19fd5a86abde642
jennifer.nord <jennifer.nord@elev.ga.ntig.se>
2024-05-13 09:42:09 +0200

    Har börjat skriva av koden

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.net.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+        throws Exception{
+
+        int portnumber = 1333;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+
     }
 }

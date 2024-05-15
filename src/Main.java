f15a0e93e54c1e1824c24bc5ac38bdbb3de4fec4
jennifer.nord <jennifer.nord@elev.ga.ntig.se>
2024-05-15 09:59:00 +0200

    Har skrivit av koden

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,20 @@ public class Main {
             portnumber = Integer.parseInt(args[0]);
         }
 
+        MulticastSocket chatMulticastSocket = new MulticastSocket(portnumber);
 
+        InetAddress group = InetAddress.getByName("255.4.5.6");
+
+        chatMulticastSocket.joinGroup(group);
+
+        String msg = "";
+        System.out.println("Type message for the server: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        msg = br.readLine();
+
+        DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
+        chatMulticastSocket.send(data);
+
+        chatMulticastSocket.close();
     }
 }
